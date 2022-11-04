e80aa59d62bb6c11ddd76acc007178d6f612fbb2
hector <hector@DESKTOP-PA1QCSS.mshome.net>
2022-11-04 21:47:15 +0100

    Hay que hacerlo bonito pero ya toma forma

--- Crisisteca/src/Ventanas/Registrarse.java
+++ Crisisteca/src/Ventanas/Registrarse.java
@@ -1,67 +1,103 @@
 package Ventanas;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.Label;
-
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextPane;
+import java.awt.*;
+import javax.swing.*;
 
 public class Registrarse extends JFrame{
 
-	private static JFrame frame;
-
 	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
 
-		frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(100, 100, 1000, 700);
-		frame.getContentPane().setLayout(new FlowLayout());
-		frame.setLocationRelativeTo(null);
+            @Override
+            public void run() {
+                new Registrarse();
+            }
+            
+		});
+
+	}
+	
+	private JTextField tfNombre;
+	private JTextField tfApellidos;
+	private JTextField tfDNI;
+	private JTextField tfDireccion;
+	private JTextField tfCodigoPostal;
+	private JTextField tfEmail;
+	private JTextField tfTelefono;
+
+	public Registrarse() {
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setBounds(100, 100, 1000, 700);
+		this.setSize(400,400);
+//		this.setSize( 600, 300 );
+		getContentPane().setLayout(new FlowLayout());
+		this.setLocationRelativeTo(null);
+		this.setTitle("Registrarse");
 
 		/// Panel de la Izquierda
+		JPanel pnlMain = new JPanel();
 		JPanel pnlIzquierda = new JPanel();
-		pnlIzquierda.setLayout(new BoxLayout(pnlIzquierda, BoxLayout.Y_AXIS));
+		JPanel pnlDerecha = new JPanel();
+	
+		pnlMain.setLayout(new BorderLayout());
+//		pnlIzquierda.setLayout(new BoxLayout(pnlIzquierda, BoxLayout.Y_AXIS));
+//		pnlDerecha.setLayout(new BoxLayout(pnlDerecha, BoxLayout.Y_AXIS));
+		pnlIzquierda.setLayout(new GridLayout(10,1));
+//		pnlIzquierda.setSize(41, 41);;
+		pnlDerecha.setLayout(new BoxLayout(pnlDerecha, BoxLayout.Y_AXIS));
 
-		JTextPane paneNombre = new JTextPane();
-		JTextPane paneApellidos = new JTextPane();
-		JTextPane paneDNI = new JTextPane();
-		JTextPane paneDireccion = new JTextPane();
-		JTextPane paneCodigoPostal = new JTextPane();
-		JTextPane paneEmail = new JTextPane();
-		JTextPane paneTelefono = new JTextPane();
-
-		pnlIzquierda.add(paneNombre);
-		pnlIzquierda.add(paneApellidos);
-		pnlIzquierda.add(paneDNI);
-		pnlIzquierda.add(paneDireccion);
-		pnlIzquierda.add(paneCodigoPostal);
-		pnlIzquierda.add(paneEmail);
-		pnlIzquierda.add(paneTelefono);
+		JLabel lblNombre = new JLabel("Nombre: ");
+		JLabel lblApellidos = new JLabel("Apellidos: ");
+		JLabel lblDNI = new JLabel("DNI: ");
+		JLabel lblDireccion = new JLabel("Dirección: ");
+		JLabel lblCodigoPostal = new JLabel("Código Postal: ");
+		JLabel lblEmail = new JLabel("E-mail: ");
+		JLabel lblTelefono = new JLabel("Teléfono: ");
+		JLabel lblCondicionesUso = new JLabel();
+		
+		pnlIzquierda.add(lblNombre);
+		pnlIzquierda.add(lblApellidos);
+		pnlIzquierda.add(lblDNI);
+		pnlIzquierda.add(lblDireccion);
+		pnlIzquierda.add(lblCodigoPostal);
+		pnlIzquierda.add(lblEmail);
+		pnlIzquierda.add(lblTelefono);
+		pnlIzquierda.add(lblCondicionesUso);
 
 		/// Panel de la Derecha
-		JPanel pnlDerecha = new JPanel();
-
-//		Label lvbl
+		tfNombre = new JTextField( "", 16 );
+		
+		pnlDerecha.add(tfNombre);
+//		pnlDerecha.add(lblApellidos);
+//		pnlDerecha.add(lblDNI);
+//		pnlDerecha.add(lblDireccion);
+//		pnlDerecha.add(lblCodigoPostal);
+//		pnlDerecha.add(lblEmail);
+//		pnlDerecha.add(lblTelefono);
 
 		
 		///Panel central
-		JPanel pnlCentro = new JPanel();
 
 		/// Panel principal
-		JPanel pnlMain = new JPanel();
-		pnlMain.setLayout(new BoxLayout(pnlMain, BoxLayout.Y_AXIS));
-		pnlDerecha.setLayout(new BoxLayout(pnlDerecha, BoxLayout.Y_AXIS));
+//		pnlMain.setLayout(new BoxLayout(pnlMain, BoxLayout.X_AXIS));
+//		pnlDerecha.setLayout(new BoxLayout(pnlDerecha, BoxLayout.Y_AXIS));
+//		pnlMain.add(pnlIzquierda, BorderLayout.WEST);
+//		pnlMain.add(pnlDerecha, BorderLayout.EAST);
+//		pnlMain.add(pnlCentro, BorderLayout.CENTER);
+		
 		pnlMain.add(pnlIzquierda, BorderLayout.WEST);
 		pnlMain.add(pnlDerecha, BorderLayout.EAST);
-		pnlMain.add(pnlCentro, BorderLayout.CENTER);
+//
+		add(pnlMain, BorderLayout.NORTH);
 
-		frame.add(pnlMain);
 
-		frame.setVisible(true);
-
+		setVisible(true);
+		
+		
+		
 	}
+	
+	
+	
 
 }
