90b8f5f1709f67e477db2700ab70ee3ca1a3419b
hector <hector@DESKTOP-PA1QCSS.mshome.net>
2022-11-04 19:55:02 +0100

    lol

--- Crisisteca/src/Ventanas/Registrarse.java
+++ Crisisteca/src/Ventanas/Registrarse.java
@@ -1,21 +1,30 @@
 package Ventanas;
-import java.awt.*;
-import javax.swing.*;
 
-public class Registrarse {
-	
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Label;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+
+public class Registrarse extends JFrame{
+
 	private static JFrame frame;
+
 	public static void main(String[] args) {
-		
+
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(100,100,1000,700);
+		frame.setBounds(100, 100, 1000, 700);
 		frame.getContentPane().setLayout(new FlowLayout());
 		frame.setLocationRelativeTo(null);
-		
-		
+
+		/// Panel de la Izquierda
 		JPanel pnlIzquierda = new JPanel();
-		
+		pnlIzquierda.setLayout(new BoxLayout(pnlIzquierda, BoxLayout.Y_AXIS));
+
 		JTextPane paneNombre = new JTextPane();
 		JTextPane paneApellidos = new JTextPane();
 		JTextPane paneDNI = new JTextPane();
@@ -23,7 +32,7 @@ public class Registrarse {
 		JTextPane paneCodigoPostal = new JTextPane();
 		JTextPane paneEmail = new JTextPane();
 		JTextPane paneTelefono = new JTextPane();
-		
+
 		pnlIzquierda.add(paneNombre);
 		pnlIzquierda.add(paneApellidos);
 		pnlIzquierda.add(paneDNI);
@@ -31,28 +40,28 @@ public class Registrarse {
 		pnlIzquierda.add(paneCodigoPostal);
 		pnlIzquierda.add(paneEmail);
 		pnlIzquierda.add(paneTelefono);
-		
-		frame.add(pnlIzquierda);
-		frame.setVisible(true);
+
+		/// Panel de la Derecha
+		JPanel pnlDerecha = new JPanel();
+
+//		Label lvbl
 
 		
-		
-		
-		
-		
-		
-		
-		
-		
+		///Panel central
+		JPanel pnlCentro = new JPanel();
+
+		/// Panel principal
+		JPanel pnlMain = new JPanel();
+		pnlMain.setLayout(new BoxLayout(pnlMain, BoxLayout.Y_AXIS));
+		pnlDerecha.setLayout(new BoxLayout(pnlDerecha, BoxLayout.Y_AXIS));
+		pnlMain.add(pnlIzquierda, BorderLayout.WEST);
+		pnlMain.add(pnlDerecha, BorderLayout.EAST);
+		pnlMain.add(pnlCentro, BorderLayout.CENTER);
+
+		frame.add(pnlMain);
+
+		frame.setVisible(true);
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
