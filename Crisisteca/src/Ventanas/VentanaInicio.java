426cbcd962c4c643aaab109dfc34af9474180c1c
asier <asier@LAPTOP-PBRV86V6>
2022-11-12 13:03:26 +0100

    Ventana Inicio

---
+++ Crisisteca/src/Ventanas/VentanaInicio.java
@@ -0,0 +1,63 @@
+package Ventanas;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class VentanaInicio extends JFrame {
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new VentanaInicio();
+			}
+		});
+	}
+	
+	public VentanaInicio() {
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+		this.getContentPane().setLayout(new BorderLayout());
+		this.setBounds(100, 100, 1000, 700);                    
+		this.setSize(400,150);  
+		getContentPane().setLayout(new FlowLayout());  
+		this.setLocationRelativeTo(null);              
+		this.setTitle("Crisisteca");                  
+		
+		//Panel texto inicio prueba
+		JPanel pnl_text = new JPanel();
+		JPanel pnl_main = new JPanel();
+		JPanel pnl_botones = new JPanel();
+		JPanel pnl_izquierda = new JPanel();
+		JPanel pnl_derecha = new JPanel();
+		
+		JLabel lblTexto = new JLabel("Bienvenido a crisisteca");
+		JButton btnRegistrarse = new JButton("Registrarse");
+		JButton btnInicSesion = new JButton("Iniciar Sesión");
+		
+		pnl_text.add(lblTexto);
+		pnl_izquierda.add(btnInicSesion);
+		pnl_derecha.add(btnRegistrarse);
+		pnl_botones.add(pnl_derecha,BorderLayout.WEST);
+		pnl_botones.add(pnl_izquierda,BorderLayout.EAST);
+		pnl_main.add(pnl_botones);
+		
+		add(pnl_text, BorderLayout.NORTH);
+		add(pnl_main, BorderLayout.SOUTH);
+		
+		setVisible(true);
+		
+	}
+}
