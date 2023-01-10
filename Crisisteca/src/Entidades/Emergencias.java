adc303526db2473703f596a5664559dbc4b7e1ef
Usuario <Usuario@LAPTOP-7NPFM5KH>
2023-01-10 12:53:16 +0100

    JavaDoc de las entidades Emergencias e Institucion

--- Crisisteca/src/Entidades/Emergencias.java
+++ Crisisteca/src/Entidades/Emergencias.java
@@ -15,39 +15,57 @@ public class Emergencias {
 		this.eReportar = eReportar;
 		this.eDetalles = eDetalles;
 	}
-
+	
+	/* return: eLugar (Lugar donde ocurre la emergencia)
+	 */
 	public String geteLugar() {
 		return eLugar;
 	}
-
+	
+	/* return: eLugar (Lugar donde ocurre la emergencia)
+	 */
 	public void seteLugar(String eLugar) {
 		this.eLugar = eLugar;
 	}
 
+	/* return: eTipoEmergencia (Tipo de emergencia que se reporta)
+	 */
 	public String geteTipoEmergencia() {
 		return eTipoEmergencia;
 	}
 
+	/* return: eTipoEmergencia (Tipo de emergencia que se reporta)
+	 */
 	public void seteTipoEmergencia(String eTipoEmergencia) {
 		this.eTipoEmergencia = eTipoEmergencia;
 	}
 
+	/* return: eReportar (Valor booleano que indica si se desea reportar la emergencia o no)
+	 */
 	public boolean iseReportar() {
 		return eReportar;
 	}
 
+	/* return: eReportar (Valor booleano que indica si se desea reportar la emergencia o no)
+	 */
 	public void seteReportar(boolean eReportar) {
 		this.eReportar = eReportar;
 	}
-
+	
+	/* return: eDetalles (String con detalles sobre la emergencia que el usuario quiera añadir)
+	 */
 	public String geteDetalles() {
 		return eDetalles;
 	}
-
+	
+	/* return: eDetalles (String con detalles sobre la emergencia que el usuario quiera añadir)
+	 */
 	public void seteDetalles(String eDetalles) {
 		this.eDetalles = eDetalles;
 	}
 
+	/* return: String con todos los parametros de la emergencia
+	 */
 	@Override
 	public String toString() {
 		return "Emergencias [eLugar=" + eLugar + ", eTipoEmergencia=" + eTipoEmergencia + ", eReportar=" + eReportar
--- Crisisteca/src/Entidades/Institucion.java
+++ Crisisteca/src/Entidades/Institucion.java
@@ -8,7 +8,6 @@ public class Institucion {
 	private int aTelefono;
 	private String aContrasenya;
 	
-	
 	public Institucion (String aCodigo, String aNombre, String aEmail, int aTelefono, String aContrasenya) {
 		this.aCodigo = aCodigo;
 		this.aNombre = aNombre;
@@ -17,68 +16,68 @@ public class Institucion {
 		this.aContrasenya = aContrasenya;
 	}
 
-	
-
+	/* return: aCodigo (Codigo propio de la institucion)
+	 */
 	public String getaCodigo() {
 		return aCodigo;
 	}
 
-
-
+	/* return: aCodigo (Codigo propio de la institucion)
+	 */
 	public void setaCodigo(String aCodigo) {
 		this.aCodigo = aCodigo;
 	}
 
-
-
+	/* return: aNombre (Nombre de la institucion)
+	 */
 	public String getaNombre() {
 		return aNombre;
 	}
 
-
-
+	/* return: aNombre (Nombre de la institucion)
+	 */
 	public void setaNombre(String aNombre) {
 		this.aNombre = aNombre;
 	}
 
-
-
+	/* return: aEmail (Email de la Institucion)
+	 */
 	public String getaEmail() {
 		return aEmail;
 	}
 
-
-
+	/* return: aEmail (Email de la Institucion)
+	 */
 	public void setaEmail(String aEmail) {
 		this.aEmail = aEmail;
 	}
 
-
-
+	/* return: aTelefono (Telefono de la Institucion)
+	 */
 	public int getaTelefono() {
 		return aTelefono;
 	}
 
-
-
+	/* return: aTelefono (Telefono de la Institucion)
+	 */
 	public void setaTelefono(int aTelefono) {
 		this.aTelefono = aTelefono;
 	}
 
-
-
+	/* return: aContrasenya (Contraseña de la Institucion)
+	 */
 	public String getaContrasenya() {
 		return aContrasenya;
 	}
 
-
-
+	/* return: aContrasenya (Contraseña de la Institucion)
+	 */
 	public void setaContrasenya(String aContrasenya) {
 		this.aContrasenya = aContrasenya;
 	}
 
-
-
+	/* return: String con todos los parametros de la Institucion
+	 */
 	@Override
 	public String toString() {
 		return "Institucion [aCodigo=" + aCodigo + ", aNombre=" + aNombre + ", aEmail=" + aEmail + ", aTelefono="
