8f849073594271001bc22ea34c73058737ee0395
Usuario <Usuario@LAPTOP-7NPFM5KH>
2023-01-10 12:32:11 +0100

    Javadoc de la entidad ciudadano

--- Crisisteca/src/Entidades/Ciudadano.java
+++ Crisisteca/src/Entidades/Ciudadano.java
@@ -2,8 +2,6 @@ package Entidades;
 
 public class Ciudadano {
 	
-	
-
 	String aNombre;
 	String aApellidos;
 	String aDNI;
@@ -26,51 +24,93 @@ public class Ciudadano {
 	public Ciudadano() {
 		
 	}
-
+	
+	/* return: aNombre (Nombre del ciudadano)
+	 */
 	public String getaNombre() {
 		return aNombre;
 	}
+	
+	/* return: aNombre (Nombre del ciudadano)
+	 */
 	public void setaNombre(String aNombre) {
 		this.aNombre = aNombre;
 	}
+	
+	/* return: aApellidos (Apellidos del ciudadano)
+	 */
 	public String getaApellidos() {
 		return aApellidos;
 	}
+	
+	/* return: aApellidos (Apellidos del ciudadano)
+	 */
 	public void setaApellidos(String aApellidos) {
 		this.aApellidos = aApellidos;
 	}
+	
+	/* return: aDNI (DNI del ciudadano)
+	 */
 	public String getaDNI() {
 		return aDNI;
 	}
+	
+	/* return: aDNI (DNI del ciudadano)
+	 */
 	public void setaDNI(String aDNI) {
 		this.aDNI = aDNI;
 	}
+	
+	/* return: aDireccion (Direccion del ciudadano)
+	 */
 	public String getaDireccion() {
 		return aDireccion;
 	}
+	
+	/* return: aDireccion (Direccion del ciudadano)
+	 */
 	public void setaDireccion(String aDireccion) {
 		this.aDireccion = aDireccion;
 	}
+	
+	/* return: aCodigoPostal (Codigo Postal del ciudadano)
+	 */
 	public int getaCodigoPostal() {
 		return aCodigoPostal;
 	}
+	
+	/* return: aCodigoPostal (Codigo Postal del ciudadano)
+	 */
 	public void setaCodigoPostal(int aCodigoPostal) {
 		this.aCodigoPostal = aCodigoPostal;
 	}
+	
+	/* return: aTelefono (Telefono del ciudadano)
+	 */
 	public int getaTelefono() {
 		return aTelefono;
 	}
+	
+	/* return: aTelefono (Telefono del ciudadano)
+	 */
 	public void setaTelefono(int aTelefono) {
 		this.aTelefono = aTelefono;
 	}
 	
+	/* return: aContasenya (Contraseña del ciudadano)
+	 */
 	public String getaContrasenya() {
 		return aContrasenya;
 	}
+	
+	/* return: aContasenya (Contraseña del ciudadano)
+	 */
 	public void setaContrasenya(String aContrasenya) {
 		this.aContrasenya = aContrasenya;
 	}
 	
+	/* return: String con todos los parametros del ciudadano
+	 */
 	@Override
 	public String toString() {
 		return "Ciudadano [aNombre=" + aNombre + ", aApellidos=" + aApellidos + ", aDNI=" + aDNI + ", aDireccion="
