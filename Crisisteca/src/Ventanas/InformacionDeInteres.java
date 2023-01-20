0e3cee23fa1c04f39abb922fc87a7fbc2bdebec0
jclau <jclau@LAPTOP-6E31SHRR>
2023-01-20 07:54:58 +0100

    empiezo una ventana con la que trabajare todo el dia, el table model craerlo en una clase a parte

---
+++ Crisisteca/src/Ventanas/InformacionDeInteres.java
@@ -0,0 +1,78 @@
+package Ventanas;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+import javax.swing.table.DefaultTableModel;
+
+public class InformacionDeInteres extends JFrame{
+	
+	
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new InformacionDeInteres();
+            }
+		});
+		
+		
+	}
+	
+	public InformacionDeInteres() {
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setTitle("Busque la información como desee");
+		this.setBounds(300, 5, 1000, 700);
+        this.setSize(700,700);
+		this.setResizable(false);
+		
+		
+		
+		JButton bOrdenar = new JButton("Ordenar busqueda");
+		JButton bFiltrar = new JButton("Filtrar busqueda");
+		JButton bLimpiar = new JButton("Limpiar busqueda");
+		
+	
+		
+		// panel de los botones de busqueda
+		JPanel pnlBotones = new JPanel();
+		pnlBotones.add(bOrdenar);
+		pnlBotones.add(bFiltrar);
+		
+		
+		
+		//panel donde se van a mostrar los datos
+		DefaultTableModel tInformacion = new DefaultTableModel();
+		tInformacion.addColumn("Codigo Postal");
+		tInformacion.addColumn("Calle");
+		tInformacion.addColumn("Usuario");
+		tInformacion.addColumn("Tipo de accidente");
+		tInformacion.addColumn("Detalles del accidente");
+		
+		
+		
+		//panel principal
+		JPanel pnlMain = new JPanel();
+		pnlMain.setLayout(new BorderLayout());
+		pnlMain.add(pnlBotones, BorderLayout.NORTH);
+		pnlMain.add(bLimpiar, BorderLayout.SOUTH);
+
+		
+		
+		getContentPane().add(pnlMain);
+		setVisible(true);
+	}
+	
+	
+	
+	
+	
+	
+
+}
