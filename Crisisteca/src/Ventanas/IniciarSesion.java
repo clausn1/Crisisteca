ecdc2e39aa46731ce11544849572ca6e57045d69
hectorparamiodeusto <113527383+hectorparamiodeusto@users.noreply.github.com>
2023-01-21 23:17:55 +0100

    no dejar espacios blancos en el JTextfild iniciarsesion

--- Crisisteca/src/Ventanas/IniciarSesion.java
+++ Crisisteca/src/Ventanas/IniciarSesion.java
@@ -18,49 +18,47 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import Principal.FuncionesEspeciales;
 
-public class IniciarSesion extends JFrame{
+public class IniciarSesion extends JFrame {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 
-            @Override
-            public void run() {
-            	
-                new IniciarSesion();
-            }
-            
+			@Override
+			public void run() {
+
+				new IniciarSesion();
+			}
+
 		});
 
 	}
-	
-	
-	
-			static Logger log;
+
+	static Logger log;
 	private JTextField tfUsuario;
 	private JTextField tfContrasenya;
-	
-	
+
 	public IniciarSesion() {
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setTitle("Iniciar sesión");
 		this.setBounds(1000, 600, 1000, 700);
-		this.setSize(600,200);
+		this.setSize(600, 200);
 		this.setResizable(false);
 		this.getContentPane().setLayout(new FlowLayout());
 		this.setLocationRelativeTo(null);
-		
-		///Paneles
+
+		/// Paneles
 		JPanel pnlMain = new JPanel();
 		JPanel pnlIzquierda = new JPanel();
 		JPanel pnlDerecha = new JPanel();
 		JPanel pnlAbajo = new JPanel();
-	
+
 		pnlMain.setLayout(new BorderLayout());
 		pnlIzquierda.setLayout(new BoxLayout(pnlIzquierda, BoxLayout.Y_AXIS));
 		pnlDerecha.setLayout(new BoxLayout(pnlDerecha, BoxLayout.Y_AXIS));
@@ -69,20 +67,20 @@ public class IniciarSesion extends JFrame{
 		lblUsuario.setFont(new Font("Arial", Font.PLAIN, 30));
 		JLabel lblContrasenya = new JLabel("Contraseña: ");
 		lblContrasenya.setFont(new Font("Arial", Font.PLAIN, 30));
-		
+
 		tfUsuario = new JTextField(15);
 		tfUsuario.setFont(new Font("Arial", Font.PLAIN, 20));
-		tfContrasenya= new JTextField(15);
+		tfContrasenya = new JTextField(15);
 		tfContrasenya.setFont(new Font("Arial", Font.PLAIN, 20));
 
 		pnlIzquierda.add(lblUsuario);
 		pnlIzquierda.add(lblContrasenya);
 		pnlDerecha.add(tfUsuario);
 		pnlDerecha.add(tfContrasenya);
-		
-        JButton bIniciarsesion = new JButton("Iniciar sesión");
-        pnlAbajo.add(bIniciarsesion);
-		bIniciarsesion.setPreferredSize(new Dimension(400,50));
+
+		JButton bIniciarsesion = new JButton("Iniciar sesión");
+		pnlAbajo.add(bIniciarsesion);
+		bIniciarsesion.setPreferredSize(new Dimension(400, 50));
 		bIniciarsesion.setFont(new Font("Arial", Font.PLAIN, 30));
 
 		pnlMain.add(pnlIzquierda, BorderLayout.WEST);
@@ -90,22 +88,31 @@ public class IniciarSesion extends JFrame{
 		pnlMain.add(pnlAbajo, BorderLayout.SOUTH);
 
 		getContentPane().add(pnlMain);
-		
+
 		this.setVisible(true);
 
 		ActionListener albIniciarsesion = new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				String Usuario = tfUsuario.getText().replaceAll(" ","") ;
-				String Contrasenya = tfContrasenya.getText().replaceAll(" ","");
-				log= Logger.getLogger("programLogger");
-				if (FuncionesEspeciales.ExisteUsuario(Usuario,Contrasenya)==0) {
-					log.log(Level.INFO, "El usuario"+Usuario+" ha iniciado sesion como Ciudadano:"+ (new Date()));
+
+				String Usuario = tfUsuario.getText().replaceAll(" ", "");
+				String Contrasenya = tfContrasenya.getText().replaceAll(" ", "");
+				log = Logger.getLogger("programLogger");
+
+				if (Usuario.isEmpty() || Contrasenya.isEmpty()) {
+					log.log(Level.INFO, "No hay contenido escrito dentro del textfield de usuario o de contraseña");
+					System.out.println("No existe ese usuario con esa contraseña");
+					JOptionPane.showMessageDialog(null, "Recuerde incluir todos los campos");
+
+				}
+
+				else if (FuncionesEspeciales.ExisteUsuario(Usuario, Contrasenya) == 0) {
+					log.log(Level.INFO, "El usuario" + Usuario + " ha iniciado sesion como Ciudadano:" + (new Date()));
 					System.out.println("Se ha iniciado sesión como Ciudadano");
 					try {
-						new ReportarEmergenciasOEmergencias(FuncionesEspeciales.devolverCiudadano(Integer.parseInt(Usuario))).setVisible(true);
+						new ReportarEmergenciasOEmergencias(
+								FuncionesEspeciales.devolverCiudadano(Integer.parseInt(Usuario))).setVisible(true);
 						setVisible(false);
 					} catch (NumberFormatException e1) {
 						log.log(Level.SEVERE, "Error en abrir la ventana de usuario de ciudadano");
@@ -118,17 +125,15 @@ public class IniciarSesion extends JFrame{
 						e1.printStackTrace();
 					}
 
-											
 				}
-				
-				
-				
-				
-				else if (FuncionesEspeciales.ExisteUsuario(Usuario,Contrasenya)==1) {
-					log.log(Level.INFO, "El usuario"+Usuario+" ha iniciado sesion como Institucion:"+ (new Date()));
+
+				else if (FuncionesEspeciales.ExisteUsuario(Usuario, Contrasenya) == 1) {
+					log.log(Level.INFO,
+							"El usuario" + Usuario + " ha iniciado sesion como Institucion:" + (new Date()));
 					System.out.println("Se ha iniciado sesión como Institucion");
 					try {
-						new InformacionORegistroCiudadanos(FuncionesEspeciales.devolverInstitucion(Usuario)).setVisible(true);
+						new InformacionORegistroCiudadanos(FuncionesEspeciales.devolverInstitucion(Usuario))
+								.setVisible(true);
 						setVisible(false);
 					} catch (NumberFormatException e1) {
 						// TODO Auto-generated catch block
@@ -143,25 +148,17 @@ public class IniciarSesion extends JFrame{
 						log.log(Level.SEVERE, "Error en abrir la ventana de usuario de Institucion");
 						e1.printStackTrace();
 					}
-				}
-				else {
+				} else {
 					log.log(null, Contrasenya);
 					log.log(Level.INFO, "Se ha intentado iniciar sesion con una contraseña que no existe");
 					System.out.println("No existe ese usuario con esa contraseña");
 				}
-				
+
 			}
-		};  
-    
+		};
+
 		bIniciarsesion.addActionListener(albIniciarsesion);
-		
-		
-		
-		
-		
-		
+
 	}
-	
-	
 
 }
